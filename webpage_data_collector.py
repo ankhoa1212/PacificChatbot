ee04c43b9272a24adea2e5deee2d03ee9fe7418a
Austin <42307214+ankhoa1212@users.noreply.github.com>
2023-09-14 23:13:40 -0700

    Add files

---
+++ webpage_data_collector.py
@@ -0,0 +1,52 @@
+import json
+import urllib.request
+from bs4 import BeautifulSoup
+
+def url_to_html(url: str, encoding = "utf8"):
+    """returns html code from url input, assuming the page is using utf8 encoding"""
+    request = urllib.request.urlopen(url)  # send url request
+    bytes = request.read()  # read bytes from url request
+    html = bytes.decode(encoding)  # convert bytes to string
+    return html
+
+def url_to_dict(url: str) -> dict:
+    """returns dictionary with webpage data from url input"""
+    soup = BeautifulSoup(url_to_html(url), 'html.parser')
+    data = {}
+    data["url"] = url
+    data["title"] = str(soup.find("title").get_text(strip=True))
+
+    # parse body into separate elements
+    body_text = []
+    for text in soup.find("body").children:
+        text = text.get_text(strip=True)
+        if text != "":
+            body_text.append(text)
+    data["body"] = body_text
+
+    # parse links into a dict: {link_text, link_url}
+    links = {}
+    for link in soup.find_all("a"):
+        url = str(link.get("href"))
+        if not (url.startswith("#") or url == ""):
+            links[link.get_text()] = url
+    data["links"] = links
+
+    return data
+
+def write_to_file(input_filename = "url_list.txt", output_filename = "data.json"):
+    """writes data from webpages and adds it into the output file
+    if url data needs to be updated, output file needs to be cleared 
+    """
+    url_list = []
+    with open(input_filename, "r") as input_file:
+        url_list = input_file.readlines()
+    all_url_data = []
+    for url in url_list:
+        url_data = url_to_dict(url)
+        all_url_data.append(url_data)
+    with open(output_filename, "a") as output_file:
+        json.dump(all_url_data, output_file, indent=4)
+
+
+write_to_file()
